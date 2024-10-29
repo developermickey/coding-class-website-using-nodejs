39f56311270d2c2b53a7f7662547bc69ec61fb81
mickeysrishti <Mukeshpathak345@gmail.com>
2024-10-29 11:23:00 +0530

    code added

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://mukeshpathak345:PG516bV1AX4CurCt@cluster0.gadvg.mongodb.net/', {
+const uri = 'mongodb+srv://mukeshpathak345:PG516bV1AX4CurCt@cluster0.gadvg.mongodb.net/myDatabase'; // Replace myDatabase with your actual database name
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // 30 seconds
 })
 .then(() => {
     console.log('Connected to MongoDB');
@@ -11,12 +14,10 @@ mongoose.connect('mongodb+srv://mukeshpathak345:PG516bV1AX4CurCt@cluster0.gadvg.
     console.error('Error connecting to MongoDB:', error);
 });
 
-
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String
-})
-
+});
 
 module.exports = mongoose.model("User", userSchema);
